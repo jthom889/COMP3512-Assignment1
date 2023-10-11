744518b0592ef924d793f65904ce5835579d1531
jthom889 <jthom889@mtroyal.ca>
2023-10-11 11:50:17 -0600

    fixed css not working

--- browse.php
+++ browse.php
@@ -1,1 +1,23 @@
-jonah
+<?php
+    require_once('./includes/config.inc.php');
+    include('./includes/helpers.inc.php');
+    include('./includes/db-classes.inc.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Browse</title>
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="songinfo.css" />
+</head>
+<body>
+    <?php
+        generateHeader();
+        generateFooter();
+    ?>
+    
+    
+
+</body>
--- homePage.php
+++ homePage.php
@@ -1,1 +1,21 @@
-j-bone
+<?php
+    require_once('./includes/config.inc.php');
+    include('./includes/helpers.inc.php');
+    include('./includes/db-classes.inc.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Home Page</title>
+</head>
+<body>
+    
+    <header>
+        
+    </header>
+    <section></section>
+    <footer></footer>
+
+</body>
