21829dd487cef2f62e0037e1a2afb21cde69635e
jthom889 <jthom889@mtroyal.ca>
2023-10-14 14:33:33 -0600

    some minor changes

--- browse.php
+++ browse.php
@@ -13,22 +13,26 @@
 
         if(!empty($_GET['title'])){
             $songs = $songGateway->getSongsByTitle($_GET['title']);
-            
+            $text = "Displaying songs with '" . $_GET['title'] . "' in the Title";
 
         } elseif(!empty($_GET['artist']) && $_GET['artist'] > 0){
             $artist = $artistGateway->getArtist($_GET['artist']);
             $songs = $songGateway->getSongsByArtist($artist[0]['artist_name']);
+            $text = "Displaying songs by " . $artist[0]['artist_name'];
             
         } elseif(!empty($_GET['genre']) && $_GET['genre'] > 0) {
             $genre = $genreGateway->getGenre($_GET['genre']);
             $songs = $songGateway->getSongsByGenre($genre[0]['genre_name']);
+            $text = "Displaying songs with " . $genre[0]['genre_name'] . " as its Genre";
            
         } elseif(!empty($_GET['year_after'])){
             $songs = $songGateway->getSongsByafter($_GET['year_after']);
+            $text = "Displaying songs released after " . $_GET['year_after'];
            
         } elseif(!empty($_GET['year_before'])){
             $songs = $songGateway->getSongsBybefore($_GET['year_before']);
-           
+            $text = "Displaying songs released before " . $_GET['year_before'];
+
         } else{
             $songs = $songGateway->getAllSongs();
         }
@@ -62,11 +66,15 @@
     <div class="headline">
         <h1>Search/Browse Results</h1>
     </div>
+    <div class="msg"><?php $text ?></div>
     <section>
+        <div class="table-container">
+            
         <?php  
             generateSongList($songs); 
              
         ?>
+        </div>
     </section>
         
     
